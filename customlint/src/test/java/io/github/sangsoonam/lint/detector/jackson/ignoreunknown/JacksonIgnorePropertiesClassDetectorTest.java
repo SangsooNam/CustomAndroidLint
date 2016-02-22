df1c785ac938a3e4895018d90be72dbfaa237e29
Sangsoo Nam <kaisia14@gmail.com>
2016-02-22 22:34:20 +0100

    Clean imports.

--- customlint/src/test/java/io/github/sangsoonam/lint/detector/jackson/ignoreunknown/JacksonIgnorePropertiesClassDetectorTest.java
+++ customlint/src/test/java/io/github/sangsoonam/lint/detector/jackson/ignoreunknown/JacksonIgnorePropertiesClassDetectorTest.java
@@ -2,13 +2,11 @@ package io.github.sangsoonam.lint.detector.jackson.ignoreunknown;
 
 import com.android.tools.lint.detector.api.Detector;
 import com.android.tools.lint.detector.api.Issue;
-import com.android.tools.lint.detector.api.TextFormat;
 import com.google.common.collect.Lists;
 
 import java.util.List;
 
 import io.github.sangsoonam.lint.AbstractDetectorTest;
-import io.github.sangsoonam.lint.detector.jackson.ignoreunknown.JacksonIgnorePropertiesClassDetector;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
